02f26e5686ac3f25f06ea7bee25cb8d2080a51bd
Allen Francis <lallenfrancisl@gmail.com>
2023-09-20 05:37:29 +0530

    deps: add dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        neutral: {
+          200: "#E5E7EB",
+          500: "#6B7280",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
