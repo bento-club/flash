05fd9a7f80becdabd9a955d9fa5766e19fe7a4dd
Allen Francis <lallenfrancisl@gmail.com>
2023-09-20 05:37:49 +0530

    feat: implement file picker

---
+++ src/components/FilePicker.vue
@@ -0,0 +1,21 @@
+<template>
+  <label
+    class="border border-neutral-200 rounded-md flex flex-col items-center justify-center p-4"
+  >
+    <div class="font-medium">Upload a file</div>
+    <div class="mt-1 text-neutral-500">PNG, JPG, PDF, GIF etc</div>
+    <input class="sr-only" type="file" :name="name" />
+  </label>
+</template>
+
+<script setup lang="ts">
+export interface FilePickerProps {
+  name?: string;
+}
+
+withDefaults(defineProps<FilePickerProps>(), {
+  name: "fileInput",
+});
+</script>
+
+<style scoped lang="scss"></style>
