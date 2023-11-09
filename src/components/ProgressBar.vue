e57e9df173cac15b27bbb959e4a470e80162b154
Allen Francis <lallenfrancisl@gmail.com>
2023-11-10 04:17:34 +0530

    deploy

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,11 +1,11 @@
 <template>
     <ProgressRoot
         :model-value="progress"
-        class="relative overflow-hidden bg-[#f3f4f6] rounded-full w-full h-2"
+        class="relative h-2 w-full overflow-hidden rounded-full bg-[#f3f4f6]"
         style="transform: translateZ(0)"
     >
         <ProgressIndicator
-            class="bg-primary-50 rounded-full w-full h-full transition-transform duration-[660ms] ease-[cubic-bezier(0.65, 0, 0.35, 1)]"
+            class="bg-primary-50 ease-[cubic-bezier(0.65, 0, 0.35, 1)] h-full w-full rounded-full transition-transform"
             :style="`transform: translateX(-${100 - (progress || 0)}%)`"
         />
     </ProgressRoot>
