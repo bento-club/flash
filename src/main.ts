c4bb5df8fbd55a6d390c3ffd314f0c9f8ba38f76
Allen Francis <lallenfrancisl@gmail.com>
2023-11-14 02:01:02 +0530

    fix: font weights not properly applied

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import "#src/assets/index.scss";
 import "@fontsource/space-grotesk/400.css";
+import "@fontsource/space-grotesk/500.css";
+import "@fontsource/space-grotesk/700.css";
 import "@fontsource/lexend/400.css";
 import App from "#src/App.vue";
 import { router } from "#src/router";
