53ca502a45990326b7b5c3acf0e9595ac02541b3
Allen Francis <lallenfrancisl@gmail.com>
2024-09-17 15:13:57 +0530

    feat(ui): add token in every request as basic auth if it exists

--- frontend/src/api/base.ts
+++ frontend/src/api/base.ts
@@ -1,7 +1,20 @@
+import { appState } from "#src/store/app"
 import ky from "ky"
 
 const api = ky.create({
     prefixUrl: "http://localhost:7000",
+    hooks: {
+        beforeRequest: [
+            (request) => {
+                if (!appState.uuid || !appState.token) {
+                    return
+                }
+
+                const token = btoa(`${appState.uuid}:${appState.token}`)
+                request.headers.set("Authorization", `Basic ${token}`)
+            },
+        ],
+    },
 })
 
 export type APIResponse<R = any> = {
